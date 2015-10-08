19464a6492340eb189c126d49f95cab05f6f911d
papertiger <papertiger8848@gmail.com>
2015-10-08 15:03:01 +0800

    add viewport

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function(){
-	gulp.src('main.scss')
+	gulp.src('styles/main.scss')
 		.pipe(sass())
 		.pipe(gulp.dest('css'))
+});
+
+gulp.task('watch', function(){
+  gulp.watch('styles/*.scss', ['sass'])
 })
